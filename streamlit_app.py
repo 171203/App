6ca3ef2388ddd30007918b50700286c2e3ea2a70
Mohan <61779254+171203@users.noreply.github.com>
2023-09-05 18:43:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,14 +32,14 @@ if st.button("Optimize"):
     # normalize it, so that some is one
     weights /= np.sum(weights)
     st.write("*****************   Markowitz Portfolio Optimization   **********************")
-    st.write('Normalized Weights : {weights.flatten()}')
+    st.write(f'Normalized Weights : {weights.flatten()}')
 
     # We generally do log return instead of return
-    Markowitz_log_ret = np.log(dataset / dataset.shift(1))
+    Markowitz_log_ret = np.log(data / data.shift(1))
 
     # Expected return (weighted sum of mean returns). Mult by 252 as we always do annual calculation and year has 252 business days
     Markowitz_exp_ret = Markowitz_log_ret.mean().dot(weights)*252
-    st.write('\nExpected return of the portfolio is : {Markowitz_exp_ret[0]}')
+    st.write(f'\nExpected return of the portfolio is : {Markowitz_exp_ret[0]}')
 
     # Exp Volatility (Risk)
     Markowitz_exp_vol = np.sqrt(weights.T.dot(252*Markowitz_log_ret.cov().dot(weights)))
