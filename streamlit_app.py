7b25e722f009b70bdecdef683ec65ea304e3706e
Mohan <61779254+171203@users.noreply.github.com>
2023-09-05 17:48:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,22 @@ st.title("Portfolio Optimization using Markowitz Model")
 # Main content
 if st.button("Optimize"):
     st.write("Optimizing...")
-    st.write(dataset)
+    st.title("Display CSV File in Streamlit App")
+
+# Add the URL of your CSV file on GitHub
+csv_url = "https://github.com/171203/App/blob/main/dataset.csv"
+
+# Load the CSV file into a DataFrame
+@st.cache
+def load_data():
+    dataset = pd.read_csv(csv_url)
+    return data
+
+dataset = load_data()
+
+# Display the loaded data
+st.write("Displaying CSV Data:")
+st.dataframe(dataset)
     # Calculate portfolio statistics
     expected_returns = dataset.pct_change().mean() * 252
     cov_matrix = dataset.pct_change().cov() * 252
