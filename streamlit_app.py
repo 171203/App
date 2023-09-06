885771c8df1aa5229f939741d6e856cbb600a39b
Mohan <61779254+171203@users.noreply.github.com>
2023-09-06 19:56:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,20 +38,6 @@ def max_sharpe_ratio():
     w0                 = np.random.dirichlet(np.full(n_assets,0.05)).tolist()    # w0 is an initila guess
 
     return minimize(negativeSR,w0,method='SLSQP', bounds  =((0,1),)*n_assets, constraints = weight_constraints)
-log_return = np.log(data / data.shift(1))
-sharpe_maximum      = max_sharpe_ratio()
-return_p,vol_p      = portfolio_performance(sharpe_maximum['x'])
-min_volatility      = min_vol()
-return_min,vol_min  = portfolio_performance(min_volatility['x'])
-portfolio        = 2673  # generation of a portfolio
-n_assets         = log_return.shape[1]
-weights          = np.random.dirichlet(np.full(n_assets,0.05),portfolio)
-mean_returns     = log_return.mean()
-sigma            = log_return.cov()
-expected_returns = np.zeros(portfolio)
-expected_vol     = np.zeros(portfolio)
-sharpe_ratio     = np.zeros(portfolio)
-rf_rate          = 0.0                          # risk free rate
 
 def log_returns(prices):
     return np.log(prices / prices.shift(1))
@@ -135,8 +121,20 @@ def efficient_portfolio_target(target):
 
 def efficient_frontier(return_range):
     return [efficient_portfolio_target(ret) for ret in return_range]
-
-
+log_return = np.log(data / data.shift(1))
+sharpe_maximum      = max_sharpe_ratio()
+return_p,vol_p      = portfolio_performance(sharpe_maximum['x'])
+min_volatility      = min_vol()
+return_min,vol_min  = portfolio_performance(min_volatility['x'])
+portfolio        = 2673  # generation of a portfolio
+n_assets         = log_return.shape[1]
+weights          = np.random.dirichlet(np.full(n_assets,0.05),portfolio)
+mean_returns     = log_return.mean()
+sigma            = log_return.cov()
+expected_returns = np.zeros(portfolio)
+expected_vol     = np.zeros(portfolio)
+sharpe_ratio     = np.zeros(portfolio)
+rf_rate          = 0.0                          # risk free rate
 
 
 # Main content
