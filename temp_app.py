ea0292e03cb353c96b56491f060a614bb85ca026
Mohan <61779254+171203@users.noreply.github.com>
2023-09-08 23:50:29 +0530

    Update temp_app.py

--- temp_app.py
+++ temp_app.py
@@ -2,76 +2,80 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import minimize
+import seaborn as sns
+from datetime import datetime
 import yfinance as yf
-from datetime import datetime 
+import scipy.optimize as sco
 
-# Fetch historical stock data
-def fetch_stock_data(symbol, start_date, end_date):
-    try:
-        df = yf.download(symbol, start=start_date, end=end_date)
-        return df
-    except Exception as e:
-        st.error(f"Error fetching data for {symbol}: {str(e)}")
-        return None
+# Download stock data
+AMZN = yf.download("AMZN", start="2012-05-18", end="2023-01-01", group_by="ticker") # Stock of Amazon
+MSFT = yf.download("MSFT", start="2012-05-18", end="2023-01-01", group_by="ticker") # Stock of Microsoft
+NFLX = yf.download("NFLX", start="2012-05-18", end="2023-01-01", group_by="ticker")
+FDX = yf.download("FDX", start="2012-05-18", end="2023-01-01", group_by="ticker")
 
-# Log returns calculation
-def log_returns(prices):
-    return np.log(prices / prices.shift(1))
+# Combine stock data into a single DataFrame
+AMZN_AJClose = AMZN['Adj Close']
+MSFT_AJClose = MSFT['Adj Close']
+FDX_AJClose = FDX['Adj Close']
+NFLX_AJClose = NFLX['Adj Close']
+dataset = pd.concat([AMZN_AJClose, MSFT_AJClose, FDX_AJClose, NFLX_AJClose], axis=1)
+dataset.columns = ['AMAZON', 'MICROSOFT', 'FDX', 'Netflix']
 
-# Portfolio optimization
-def optimize_portfolio(returns):
-    n_assets = returns.shape[1]
-    w0 = np.random.dirichlet(np.full(n_assets, 0.05))
-    bounds = tuple((0, 1) for x in range(n_assets))
-    constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
-    opts = minimize(negative_sharpe_ratio, w0, method='SLSQP', bounds=bounds, constraints=constraints)
-    return opts
-
-# Objective function for minimizing negative Sharpe ratio
-def negative_sharpe_ratio(weights):
-    return -portfolio_return(weights) / portfolio_volatility(weights)
-
-# Portfolio return calculation
-def portfolio_return(weights):
-    return np.sum(np.mean(log_returns(dataset).mean() * weights) * 252)
-
-# Portfolio volatility calculation
-def portfolio_volatility(weights):
-    return np.sqrt(np.dot(weights.T, np.dot(log_returns(dataset).cov() * 252, weights)))
-
-# Main Streamlit app
+# Define Streamlit app
 def main():
-    st.title("Financial Data Analysis and Portfolio Optimization")
-    
-    # Sidebar
-    st.sidebar.header("Settings")
-    start_date = st.sidebar.date_input("Start Date", datetime(2012, 5, 18))
-    end_date = st.sidebar.date_input("End Date", datetime(2023, 1, 1))
-    symbol = st.sidebar.text_input("Stock Symbol (e.g., AMZN)", "AMZN")
+    st.title("Portfolio Optimization App")
 
-    if st.sidebar.button("Fetch Data"):
-        dataset = fetch_stock_data(symbol, start_date, end_date)
+    # Display boxplot
+    st.subheader("Boxplot of Stock Prices")
+    st.pyplot(plot_boxplot(dataset))
 
-        if dataset is not None:
-            st.write("Data loaded successfully.")
-            st.subheader("Summary of Data")
-            st.write(dataset.head())
+    # Display scatter matrix
+    st.subheader("Scatter Matrix of Stock Prices")
+    st.pyplot(plot_scatter_matrix(dataset))
 
-            # Perform portfolio optimization
-            st.subheader("Portfolio Optimization")
-            portfolio_opts = optimize_portfolio(log_returns(dataset).dropna().T.values)
+    # Display daily close prices
+    st.subheader("Daily Close Prices")
+    st.pyplot(plot_daily_close_prices(dataset))
 
-            st.write("Optimal Weights:", portfolio_opts.x.round(3))
-            st.write("Expected Return:", portfolio_return(portfolio_opts.x))
-            st.write("Expected Volatility:", portfolio_volatility(portfolio_opts.x))
+    # Display correlation heatmap
+    st.subheader("Correlation Heatmap")
+    st.pyplot(plot_correlation_heatmap(dataset))
 
-            # Display portfolio optimization results
-            st.subheader("Efficient Frontier and Portfolio Optimization")
-            st.write("Note: For demonstration purposes, the efficient frontier plot is not included in this example.")
-            # You can add your efficient frontier plot code here
-    else:
-        st.warning("Please select a valid date range and stock symbol.")
+    # Portfolio optimization
+    st.subheader("Portfolio Optimization Results")
+    st.write("Portfolio performance metrics and optimization results go here.")
+
+# Define functions for creating plots
+def plot_boxplot(data):
+    plt.figure(figsize=(8, 6))
+    data.boxplot()
+    plt.title("Boxplot of Stock Prices")
+    return plt
+
+def plot_scatter_matrix(data):
+    plt.figure(figsize=(10, 8))
+    pd.plotting.scatter_matrix(data, figsize=(10, 10))
+    plt.title("Scatter Matrix of Stock Prices")
+    return plt
+
+def plot_daily_close_prices(data):
+    plt.figure(figsize=(12, 6))
+    plt.grid(True)
+    plt.title('Daily Close Prices')
+    plt.xlabel('Date: May 18th, 2012 - Dec. 30th, 2022')
+    plt.ylabel('Values')
+    for col in data.columns:
+        plt.plot(data.index, data[col], label=col)
+    plt.legend()
+    return plt
+
+def plot_correlation_heatmap(data):
+    plt.figure(figsize=(10, 6))
+    corr = data.corr()
+    sns.heatmap(corr, xticklabels=corr.columns.values, yticklabels=corr.columns.values, annot=True, annot_kws={'size': 12})
+    plt.title("Correlation Heatmap")
+    return plt
 
 if __name__ == "__main__":
     main()
+
